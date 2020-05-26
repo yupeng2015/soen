ae6b0fb100eb887c1c5d3075a49491a83a59f5b2
soen <970852318@qq.com>
2020-05-26 18:24:52 +0800

    feat 忽略文件

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 $webDir = dirname(__DIR__);
 require_once $webDir . '/vendor/autoload.php';
-require_once $webDir . '/vendor/soen/server/src/Main.php';
-require_once $webDir . '/config/routes.php';
-(new \Soen\Server\Main())->run();
+//require_once $webDir . '/vendor/soen/server/src/Main.php';
+//require_once $webDir . '/config/routes.php';
+//(new \Soen\Server\Main())->run();
+use Soen\Filesystem\File;
+$files = new File('./test');
+$config = $files->readArrayFilesDeep($files);
+print_r($config);
