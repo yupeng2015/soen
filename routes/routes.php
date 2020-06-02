3465f4c848e833f86705c4c1b421bf359901a763
soen <970852318@qq.com>
2020-06-02 18:27:24 +0800

    feat:router

--- routes/routes.php
+++ routes/routes.php
@@ -3,9 +3,12 @@
 use Soen\Router\Router;
 use Soen\Http\Message\Request;
 use Soen\Http\Message\Response;
-Router::addRoute(['get'], '/index', [App\Controllers\IndexController::class, 'index'], function (){
-	return [
-		\App\Middlewares\TestMiddlewares::class,
-		\App\Middlewares\Test1Middlewares::class,
-	];
-});
+//Router::add(['get'], '/index', [App\Controllers\IndexController::class, 'index'], [
+//		\App\Middlewares\TestMiddlewares::class,
+//		\App\Middlewares\Test1Middlewares::class,
+//	]);
+Router::group(['middle' => [\App\Middlewares\TestMiddlewares::class,\App\Middlewares\Test1Middlewares::class]], [
+        Router::get('/test', [App\Controllers\IndexController::class, 'test'], [\App\Middlewares\TestMiddlewares::class]),
+        Router::get('/index', [App\Controllers\IndexController::class, 'index'], [\App\Middlewares\Test1Middlewares::class]),
+    ]
+);
